eac5af1a3eff6498fc8470117ecf7dc2f372e45f
EC2 Default User <ec2-user@ip-172-31-40-255.ec2.internal>
2020-11-02 13:01:38 +0000

    add comment function

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
 
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 200 }
   validates :body, presence: true, length: { maximum: 200 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+  end
+  
   delete "books/:id" => "books#destroy"
   resources :users
   resources :top
